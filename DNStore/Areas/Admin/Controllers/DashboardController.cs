708f4c2878d6ae9a55f756bbf9fd010cab5a10d5
NgojcSown <107361006+NgojcSown@users.noreply.github.com>
2023-02-28 15:35:07 +0700

    Add project files.

---
+++ DNStore/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using DNStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DNStore.Areas.Admin.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        private DNStoreDbContext db = new DNStoreDbContext();
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            ViewBag.CountOrderSuccess = db.Orders.Where(m => m.Status == 3).Count();
+            ViewBag.CountOrderCancel = db.Orders.Where(m => m.Status == 1).Count();
+            ViewBag.CountContactDoneReply = db.Contacts.Where(m => m.Flag == 0).Count();
+            ViewBag.CountUser = db.Users.Where(m => m.Status != 0 && m.Access==0).Count();
+            return View();
+        }
+    }
+}
---
+++ DNStore/Library/ModelThongbao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DNStore.Library
+{
+    public class ModelThongbao
+    {
+        public string msg { get; set; }
+        public String msg_type { get; set; }
+    }
+}
